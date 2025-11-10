148e8dcae2e0ab2e218fd55d8db89a548f00d8dd
Виталии <Lexus089aa@gmail.com>
2025-11-10 14:51:36 +0800

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@ const modalWindow = document.querySelector('.modal');
 const buttonModals = document.querySelectorAll('.modal__button');
 const modalClose = document.querySelector('.modal__close');
 const body = document.querySelector('body');
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('.header__nav');
-const body = document.querySelector('body');
 
 buttonModals.forEach((item) => {
     item.addEventListener('click', () => {
@@ -27,9 +24,3 @@ modalClose.addEventListener('click', () => {
     body.classList.remove('noscroll');
 
 })
-
-burger.addEventListener('click', () => {
-  burger.classList.toggle('active');
-  nav.classList.toggle('active');
-  body.classList.toggle('noscroll');
-});
